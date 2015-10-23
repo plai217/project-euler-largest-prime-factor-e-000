922382081375176c78bc581af235e27f0b7ede6f
plai217 <philiplai217@gmail.com>
2015-10-23 19:06:04 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,33 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+
+  array = findfactors(num).sort.reverse
+  array.each  {|x| if prime(x) then return x end} 
+
+end
+
+def prime(num)
+
+  Math.sqrt(num).ceil.downto(4) do |x|
+    if num % (x-1) == 0 
+      return false
+    end
+  end
+  return true
+
+end
+
+
+def findfactors(num)
+
+  factors = []
+
+  Math.sqrt(num).ceil.downto(4) do |x|
+    if num % (x-1) == 0 
+      factors << (x-1)
+    end
+  end
+  factors
+end
+
+largest_prime_factor(490)
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,34 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    array = findfactors.sort.reverse
+    array.each  {|x| if prime(x) then return x end} 
+  end
+
+  def prime(num)
+      Math.sqrt(num).ceil.downto(4) do |x|
+        if num % (x-1) == 0 
+          return false
+        end
+      end
+    return true
+  end
+
+
+  def findfactors
+    factors = []
+    Math.sqrt(@num).ceil.downto(4) do |x|
+      if @num % (x-1) == 0 
+        factors << (x-1)
+      end
+    end
+    factors
+  end
+
+
+end
